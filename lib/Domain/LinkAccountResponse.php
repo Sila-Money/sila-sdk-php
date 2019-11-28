df6f0ed1471d49ac0e20d17815ab73dbe9d199a1
José Morales <jmorales@digitalgeko.com>
2019-11-28 17:14:07 -0600

    Added LinkAccountResponse model

---
+++ lib/Domain/LinkAccountResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Link Account Response
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Domain;
+
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Link Account Response
+ * Object used to map Link Account response.
+ * @category Class
+ * @package  Silamoney\Client
+ * @author   José Morales <jmorales@digitalgeko.com>
+ */
+class LinkAccountReponse
+{
+    /**
+     * @var string
+     * @Type("string")
+     */
+    private $status;
+
+    /**
+     * Gets the response status.
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+}
