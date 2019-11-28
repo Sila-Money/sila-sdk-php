2e076b2633438fa2213351c507a8162d6151d678
José Morales <jmorales@digitalgeko.com>
2019-11-28 15:47:12 -0600

    Added Environments enum class

---
+++ lib/Domain/Environments.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Environments
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Domain;
+
+/**
+ * Environments
+ * Enum used to get accepted Sila environments.
+ * @category Class
+ * @package  Silamoney\Client
+ * @author   José Morales <jmorales@digitalgeko.com>
+ */
+namespace Silamoney\Client\Domain;
+
+class Environments extends SplEnum
+{
+    private const __default = self::SANDBOX;
+    /**
+     * Sila sandbox environment.
+     */
+    public const SANDBOX = "https://sandbox.silamoney.com/0.2";
+    /**
+     * Sila production environment.
+     */
+    public const PRODUCTION = "https://api.silamoney.com/0.2";
+}
