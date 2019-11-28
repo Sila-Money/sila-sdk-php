785ebf40fd74bc4579edd9eeb6b5446d7bd3e12b
José Morales <jmorales@digitalgeko.com>
2019-11-28 12:30:19 -0600

    Added BaseResponse class

---
+++ lib/Domain/BaseResponse.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Address
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Domain;
+
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Address
+ * Object used in the entity message.
+ * @category Class
+ * @package  Silamoney\Client
+ * @author   José Morales <jmorales@digitalgeko.com>
+ */
+class BaseResponse
+{
+    /**
+     * Reference
+     * @var string
+     */
+    private $reference;
+
+    /**
+     * Message
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Status
+     * @var string
+     */
+    private $status;
+
+    /**
+     * Gets the response reference.
+     * @return string
+     */
+    public function getReference(): string
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Gets the response message.
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Gets the response status.
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+}
