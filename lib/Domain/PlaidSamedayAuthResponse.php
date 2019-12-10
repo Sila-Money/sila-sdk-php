2fd1e6e6abd4264630ba7986cdcb699a7af4e8d7
José Morales <jmorales@digitalgeko.com>
2019-12-10 14:45:29 -0600

    Added Plaid Sameday Auth endpoint Added 200 test case to Plaid Sameday Auth endpoint

---
+++ lib/Domain/PlaidSamedayAuthResponse.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Plaid Sameday Auth Response
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Domain;
+
+use JMS\Serializer\Annotation\Type;
+use Respect\Validation\Validator as v;
+
+/**
+ * Plaid Sameday Auth Response
+ * Object used to map the plaid sameday auth method response.
+ * @category Class
+ * @package  Silamoney\Client
+ * @author   José Morales <jmorales@digitalgeko.com>
+ */
+class PlaidSamedayAuthResponse
+{
+    /**
+     * Public Token
+     * @var string
+     * @Type("string")
+     */
+    private $publicToken;
+
+    /**
+     * Message
+     * @var string
+     * @Type("string")
+     */
+    private $message;
+
+    /**
+     * Status
+     * @var string
+     * @Type("string")
+     */
+    private $status;
+
+    /**
+     * Gets the response reference.
+     * @return string
+     */
+    public function getPublicToken(): string
+    {
+        return $this->publicToken;
+    }
+
+    /**
+     * Gets the response message.
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Gets the response status.
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+}
---
+++ test/Api/PlaidSamedayAuthTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Sila Balance Test
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Api;
+
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\ {
+    Request,
+    Response
+};
+use JMS\Serializer\SerializerBuilder;
+use PHPUnit\Framework\TestCase;
+use Silamoney\Client\Domain\{
+    BalanceEnvironments,
+    SilaBalanceMessage
+};
+
+/**
+ * Sila Balance Test
+ * Tests for the sila balance endpoint in the Sila Api class.
+ *
+ * @category Class
+ * @package Silamoney\Client
+ * @author José Morales <jmorales@digitalgeko.com>
+ */
+class PlaidSamedayAuthTest extends TestCase
+{
+    /**
+     *
+     * @var \Silamoney\Client\Api\ApiClient
+     */
+    protected static $api;
+
+    /**
+     *
+     * @var \Silamoney\Client\Utils\TestConfiguration
+     */
+    protected static $config;
+
+    /**
+     *
+     * @var \JMS\Serializer\SerializerBuilder
+     */
+    private static $serializer;
+
+    public static function setUpBeforeClass(): void
+    {
+        \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
+        self::$serializer = SerializerBuilder::create()->build();
+        $json = file_get_contents(__DIR__ . '/Data/Configuration.json');
+        self::$config = self::$serializer->deserialize($json, 'Silamoney\Client\Utils\TestConfiguration', 'json');
+        self::$api = SilaApi::fromDefault(self::$config->appHandle, self::$config->privateKey);
+    }
+
+    /**
+     * @test
+     */
+    public function testPlaidSamedayAuth200()
+    {
+        $body = file_get_contents(__DIR__ . '/Data/PlaidSamedayAuth200.json');
+        $mock = new MockHandler([
+            new Response(200, [], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        self::$api->getApiClient()->setApiHandler($handler);
+        $response = self::$api->plaidSamedayAuth(self::$config->userHandle, "Custom Account Name");
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Plaid public token succesfully created", $response->getData()->getMessage());
+        $this->assertEquals("SUCCESS", $response->getData()->getStatus());
+        $this->assertEquals("token", $response->getData()->getPublicToken());
+    }
+}
