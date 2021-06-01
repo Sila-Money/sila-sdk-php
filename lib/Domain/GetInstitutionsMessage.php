656bfa1e2c28b25107ccb2583a342fad57de6fc7
AlferOro <aorozco@digitalgeko.com>
2021-06-01 01:58:00 -0500

    Added message class for /get_institutions.

---
+++ lib/Domain/GetInstitutionsMessage.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Get Institutions Message
+ * PHP version 7.2
+ */
+
+namespace Silamoney\Client\Domain;
+
+use JMS\Serializer\Annotation\Type;
+use Respect\Validation\Validator as v;
+
+/**
+ * Get Institutions Message
+ * Object sent in the Get Institutions method.
+ * @category Class
+ * @package  Silamoney\Client
+ * @author   Alanfer Orozco <aorozco@digitalgeko.com>
+ */
+class GetInstitutionsMessage implements ValidInterface
+{
+    /**
+     * @var Silamoney\Client\Domain\Header
+     * @Type("Silamoney\Client\Domain\Header")
+     */
+    private $header;
+
+    /**
+     * @var string
+     * @Type("string")
+     */
+    private $message;
+
+    /**
+     * @var Silamoney\Client\Domain\SearchFilters
+     * @Type("Silamoney\Client\Domain\SearchFilters")
+     */
+     private $searchFilters;
+
+    /**
+     * Constructor for GetInstitutionsMessage object.
+     * @param string $appHandle
+     * @param Silamoney\Client\Domain\SearchFilters $searchFilters
+     */
+     public function __construct(
+        string $appHandle,
+        SearchFilters $searchFilters
+    ) {
+        $this->searchFilters = $searchFilters;
+        $this->message = Message::HEADER;
+        $this->header = new Header($appHandle);
+    }
+
+    public function isValid(): bool
+    {
+        return v::notOptional()->validate($this->header)
+            && $this->header->isValid()
+            && v::stringType()->notEmpty()->validate($this->message)
+            && ($this->searchFilters === null || $this->searchFilters->isValid());
+    }
+}
